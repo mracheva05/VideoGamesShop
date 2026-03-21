92468af1a5a563da6f63edc06d54d9a7d338ed7a
Miha <stu2401261002@uni-plovdiv.bg>
2026-03-21 14:53:34 +0200

    init

---
+++ src/MainForm.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class MainForm {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTable tableClients;
+    private JTextField textClientName;
+    private JTextField textClientPhone;
+    private JButton btnAddClient;
+    private JButton btnDeleteClient;
+    private JTable tableGames;
+    private JTextField textGameTitle;
+    private JTextField txtGamePrice;
+    private JTextField txtGameGenre;
+    private JButton btnAddGame;
+    private JButton btnDeleteGame;
+    private JTextField textClientCountry;
+    private JComboBox comboSearchClient;
+    private JComboBox comboSearchGame;
+    private JButton btnSearch;
+    private JTable tableSearch;
+    private JTable tableOrders;
+    private JComboBox comboOrderClient;
+    private JComboBox comboOrderGame;
+    private JButton btnDeleteOrder;
+    private JButton btnAddOrder;
+    private JButton btnUpdateClient;
+    private JButton btnUpdateGames;
+}
